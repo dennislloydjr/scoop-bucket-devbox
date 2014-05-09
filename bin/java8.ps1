48377a55126d3b8ea6009ff56690935b6270e4a8
Dennis Lloyd Jr <dennislloydjr@gmail.com>
2014-05-08 21:07:02 -0400

    Remove that java installer exe after completing the install.

--- bin/java8.ps1
+++ bin/java8.ps1
@@ -3,3 +3,4 @@ $version = "8u5"
 $build = "b13"
 Invoke-Expression "cmd /c wget --no-check-certificate --no-cookies --header 'Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/$version-$build/jdk-$version-windows-x64.exe -O $downloadTarget"
 Invoke-Expression "cmd /c $downloadTarget /s INSTALLDIR=`"$PSScriptRoot`""
+rm $downloadTarget
