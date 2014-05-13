3b878b7f167038ec0624db6956d6bf0ca4c298e3
Dennis Lloyd Jr <dennislloydjr@gmail.com>
2014-05-12 22:49:51 -0400

    Parameterize the java8 download and install script.

--- bin/java8.ps1
+++ bin/java8.ps1
@@ -1,6 +1,6 @@
+param([String] $version, [String] $build)
+
 $downloadTarget = "java8-install.exe"
-$version = "8u5"
-$build = "b13"
 Invoke-Expression "cmd /c wget --no-check-certificate --no-cookies --header 'Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/$version-$build/jdk-$version-windows-x64.exe -O $downloadTarget"
 Invoke-Expression "cmd /c $downloadTarget /s INSTALLDIR=`"$PSScriptRoot`""
 rm $downloadTarget
